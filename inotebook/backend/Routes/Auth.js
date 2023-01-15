d0fc4860caae28f46560a70183fd9c5b96230388
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-01-16 03:47:13 +0530

    Validation for user email has been completed

--- inotebook/backend/Routes/Auth.js
+++ inotebook/backend/Routes/Auth.js
@@ -4,12 +4,18 @@ const User=require("../Models/User");
 const { body, validationResult } = require('express-validator');
 
 //Create a user using: POST "/api/auth/". Doesn't require Authentication    
-router.post('/',(req,res)=>{
-    
-    console.log(req.body);
-    const user=User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/',[body('email').isEmail(),body('name').isLength({min : 3}),body('password').isLength({min: 5})],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user))
+      .catch(err=>{console.log(err)   
+    res.json({error: "Please enter unique value for email",message:err.message})})
 })
 
 module.exports=router;
--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURI='mongodb://localhost:27017';
+const mongoURI='mongodb://localhost:27017/inotebook';
 mongoose.set("strictQuery", false);
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,()=>{
