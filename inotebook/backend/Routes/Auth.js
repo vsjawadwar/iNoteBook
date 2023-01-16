8e60512ed81577645417fd0c4039bc9283ccf329
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-01-17 04:57:58 +0530

    Added bcryptjs and jsonwebtoken for security purpose also learned about hashing salt

--- inotebook/backend/Routes/Auth.js
+++ inotebook/backend/Routes/Auth.js
@@ -2,7 +2,9 @@ const express=require('express');
 const router=express.Router();
 const User=require("../Models/User");
 const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcrypt');
+const jwt=require('jsonwebtoken');
+const JWT_SECRET='Vishalisagoodbo#y';
 //Create a user using: POST "/api/auth/createuser". 
 // Doesn't require Authentication    
 // No Login Required
@@ -29,12 +31,23 @@ router.post('/createuser',[
         if(user){
             return res.status(400).json({error: "Sorry.. User with this email already exists"});
         }
+
+        const salt=await bcrypt.genSalt(10);
+        const secPass= await bcrypt.hash(req.body.password,salt);
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPass,
         })
-        res.json(user);
+        const data={
+            user: {
+                id: user.id
+            }
+        }
+        const authToken=jwt.sign(data,JWT_SECRET);
+        console.log(authToken);
+        // res.json(user);
+        res.json({authToken})
     }catch(error){
         console.error(error.message);
         res.status(500).send("Some error occured");
