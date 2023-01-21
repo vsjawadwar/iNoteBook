583ca86437196cee44c6601c15a2ef0fdb3e0da2
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-01-21 08:50:07 +0530

    DeleteNote Endpoint added and tested successfully along with all validations

--- inotebook/backend/Routes/Note.js
+++ inotebook/backend/Routes/Note.js
@@ -18,7 +18,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 
 //Route 2: Add n new note by using: POST "/api/notes/addnote". Login Required
 router.post('/addnote', fetchuser, [
-    body('title', 'Enter a valid email').isLength({min:3}),
+    body('title', 'Enter a valid email').isLength({ min: 3 }),
     body('description', 'Description must be atleast 5 characters').isLength({ min: 10 })
 ], async (req, res) => {
     try {
@@ -41,36 +41,56 @@ router.post('/addnote', fetchuser, [
 
 //Route 3: Update an existing note by using: PUT "/api/notes/updatenote". Login Required
 router.put('/updatenote/:id', fetchuser, [
-    body('title', 'Enter a valid email').isLength({min:3}),
+    body('title', 'Enter a valid email').isLength({ min: 3 }),
     body('description', 'Description must be atleast 5 characters').isLength({ min: 10 })
 ], async (req, res) => {
-    const {title,description,tag}=req.body;
-    
-    //create new note object
-    const newNote={};
-    if(title){
-        newNote.title=title
+    const { title, description, tag } = req.body;
+    try {
+        //create new note object
+        const newNote = {};
+        if (title) {
+            newNote.title = title
+        }
+        if (description) {
+            newNote.description = description
+        }
+        if (tag) {
+            newNote.tag = tag
+        }
+        //Find the note to be updated and update it.
+        let note = await Note.findById(req.params.id); //Need to use await or else it will goes to next line we will get error because of undefined value.
+        if (!note) {
+            return res.status(401).send("Not Found");
+        }
+        if (note.user.toString() != req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json({ note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error: Some error occured");
     }
-    if(description){
-        newNote.description=description
-    }
-    if(tag){
-        newNote.tag=tag
-    }
-
-    //Find the note to be updated and update it.
-    let note= await Note.findById(req.params.id); //Need to use await or else it will goes to next line we will get error because of undefined value.
-    if(!note){
-        return res.status(401).send("Not Found");
-    }
-    if(note.user.toString() != req.user.id)
-    {
-        return res.status(401).send("Not Allowed");
-    }
-
-    note= await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true});
-    res.json({note});
 });
 
-
+//Route 4: Delete an existing note by using: DELETE "/api/notes/deletenote". Login Required
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    try {
+        //Find the note to be deleted and delete it.
+        let note = await Note.findById(req.params.id); //Need to use await or else it will goes to next line we will get error because of undefined value.
+        if (!note) {
+            return res.status(401).send("Not Found");
+        }
+        //Allow deletion only if that not was created by same logged user
+        if (note.user.toString() != req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+        note = await Note.findByIdAndDelete(req.params.id);
+        res.json({ "Sucess": "Note has been deleted", note: note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error: Some error occured");
+    }
+});
 module.exports = router;
